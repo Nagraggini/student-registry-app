ef544e4ca639ff5b228e6407ab1237aa1f9802e1
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2026-02-21 09:23:20 +0100

    Now ready to run.

--- src/main/java/com/example/student_registry_app/StudentRegistryAppApplication.java
+++ src/main/java/com/example/student_registry_app/StudentRegistryAppApplication.java
@@ -3,6 +3,8 @@ package com.example.student_registry_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+Start gomb és http://localhost:8080/ a böngészőben. */
 @SpringBootApplication
 public class StudentRegistryAppApplication {
 	// ! DTO-t megvalósítani.
